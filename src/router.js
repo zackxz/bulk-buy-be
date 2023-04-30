54bcf3b2f57726762488257c8daa54ff8011a3d6
zackxz <limkm@live.com>
2023-04-30 15:12:15 +0800

    Updated GH workflow to restart pod

--- src/router.js
+++ src/router.js
@@ -6,4 +6,8 @@ router.get("/", (req, res) => {
   res.send("respond with a resource");
 });
 
+router.get("/ping", (req, res) => {
+  res.send("The server is reached successfully.");
+});
+
 module.exports = router;
